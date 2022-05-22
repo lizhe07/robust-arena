4769efa5fc03d83cb2de3f00a4ae047226733e16
Zhe Li <zhe.li@bcm.edu>
2022-05-22 16:29:43 -0500

    Update requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 15 16:35:37 2020
-
-@author: Zhe
-"""
-
 from roarena import __version__
 from setuptools import setup, find_packages
 
@@ -14,5 +7,5 @@ setup(
     author='Zhe Li',
     python_requires='>=3',
     packages=find_packages(),
-    install_requires=['torch', 'jarvis', 'eagerpy', 'numba<0.53', 'foolbox'],
+    install_requires=['torch', 'jarvis', 'eagerpy', 'numba', 'foolbox'],
 )
