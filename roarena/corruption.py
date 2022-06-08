997c225945b2ab13ebf1bf0a68bcf5b569d848fd
zhe <zhe@at-rotgpu3.localdomain>
2022-06-07 21:33:12 -0500

    Update CorruptionJob plots

--- roarena/corruption.py
+++ roarena/corruption.py
@@ -1,5 +1,7 @@
 import os, argparse, torch, time, random, json
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 from torchvision.transforms.functional import rgb_to_grayscale
@@ -16,7 +18,7 @@ CORRUPTIONS = [
     'defocus_blur', 'glass_blur', 'motion_blur', 'zoom_blur',
     'snow', 'frost', 'fog', 'brightness',
     'contrast', 'elastic_transform', 'pixelate', 'jpeg_compression',
-    ]
+]
 SEVERITIES = [1, 2, 3, 4, 5]
 
 
@@ -152,7 +154,7 @@ class CorruptionJob(BaseJob):
         preview = {'loss': loss, 'acc': acc}
         return ckpt, preview
 
-    def summarize(self, model_pths, severity=5):
+    def summarize(self, model_paths, severity=5):
         r"""Summarizes a list of models.
 
         Args
@@ -169,65 +171,135 @@ class CorruptionJob(BaseJob):
             array, containing testing accuracies of each model.
 
         """
-        accs = {}
-        for corruption in CORRUPTIONS:
-            accs[corruption] = []
-            for model_pth in model_pths:
+        accs = []
+        for model_path in model_paths:
+            print(f"Fetching corruption robustness results for {model_path}...")
+            _accs = {}
+            for corruption in CORRUPTIONS:
                 config = {
-                    'model_pth': model_pth,
+                    'model_path': model_path,
                     'corruption': corruption,
                     'severity': severity,
-                    }
-                key = self.configs.get_key(config)
-                if key is not None and self.is_completed(key):
-                    accs[corruption].append(self.results[key]['acc'])
-            accs[corruption] = np.array(accs[corruption])
-        accs['clean'] = []
-        for model_pth in model_pths:
-            config = {
-                'model_pth': model_pth,
-                'corruption': None,
-                'severity': 0,
                 }
-            result, _ = self.process(config, verbose=False)
-            accs['clean'].append(result['acc'])
-        accs['clean'] = np.array(accs['clean'])
+                _, ckpt, _ = self.load_ckpt(config)
+                _accs[corruption] = ckpt['acc']
+            _accs['clean'] = torch.load(model_path)['acc']
+            accs.append(_accs)
         return accs
 
-    def plot_comparison(self, ax, groups, accs):
-        r"""Plots comparison of groups.
+    def plot_full(self, accs, ax=None, bar_width=0.8, colors=None, legends=None):
+        r"""Plots full comparison of groups of models.
+
+        Classification accuracy for each type of corruption is compared. If one
+        group contains more than one model, standard deviation across models are
+        shown as errorbars.
 
         Args
         ----
-        ax: matplot axis
-            The axis for plotting.
-        groups: list
-            Each item is a tuple of `(tag, model_pths, color)`. `tag` is the
-            label for the group, `model_pths` is the list of model pths and
-            `color` is a color tuple of shape `(3,)`.
-        accs: list
-            Each item is a dictionary returned by `summarize`.
+        accs: list[dict[str, Union[float, list[float]]]]
+            Model(s) performance for each corruption at a severity level.
+        ax:
+            Matplotlib axis for plotting.
+        bar_width: float
+            Width parameter for bar plots, value in (0, 1).
+        colors: Optional[list[tuple[float]]]
+            A list of RGB values, specifying colors for each group.
+        legends: list[str]
+            Legends for each group.
 
         """
-        bin_width = 0.8/len(groups)
-        bars, legends = [], []
+        if ax is None:
+            _, ax = plt.subplots(figsize=(8, 3))
+        num_groups = len(accs)
+        bar_width /= num_groups
         _CORRUPTIONS = CORRUPTIONS+['clean']
-        xticks = np.array(list(range(len(CORRUPTIONS)))+[len(CORRUPTIONS)+1])
-        for i, (tag, _, color) in enumerate(groups):
+        xticks = np.arange(len(_CORRUPTIONS))
+        if colors is None:
+            colors = [matplotlib.cm.get_cmap('tab10')(x)[:3] for x in np.linspace(0, 1, num_groups)]
+        if legends is None:
+            legends = [f'Group {i}' for i in range(num_groups)]
+        bars = []
+        for i in range(num_groups):
+            color = colors[i]
             acc_mean = np.array([np.mean(accs[i][c]) for c in _CORRUPTIONS])*100
             acc_std = np.array([np.std(accs[i][c]) for c in _CORRUPTIONS])*100
             h = ax.bar(
-                xticks+(i-0.5*(len(groups)-1))*bin_width,
-                acc_mean, width=bin_width, yerr=acc_std, zorder=2, facecolor=color,
-                )
-            h.errorbar.get_children()[0].set_edgecolor(np.array(color)*0.6)
+                xticks+(i-0.5*(num_groups-1))*bar_width,
+                acc_mean, width=bar_width, yerr=acc_std, zorder=2, facecolor=color,
+            )
+            h.errorbar.get_children()[0].set_edgecolor(np.array(color)*0.8+0.2)
             bars.append(h)
-            legends.append(tag)
         ax.legend(bars, legends)
+        ylim = max(max(accs[i].values()) for i in range(num_groups))*100
+        ylim = -(-ylim//10)*10
         ax.set_xticks(xticks)
         ax.set_xticklabels(_CORRUPTIONS, rotation=90)
-        ax.set_ylabel('accuracy (%)')
-        ax.set_ylim([0, 100])
+        ax.set_ylabel('Accuracy (%)')
+        ax.set_ylim([0, ylim])
+        ax.grid(axis='y')
+
+    def plot_digest(self, accs, ax=None, bar_width=0.8, colors=None, legends=None):
+        r"""Plots digest comparison of groups of models.
+
+        Classification accuracy for 'low', 'medium' and 'high' corruptions is
+        compared. Standard deviation computed over in-category corruptions and
+        in-group models are shown as error bars.
+
+        Args
+        ----
+        accs: list[dict[str, Union[float, list[float]]]]
+            Model(s) performance for each corruption at a severity level.
+        ax:
+            Matplotlib axis for plotting.
+        bar_width: float
+            Width parameter for bar plots, value in (0, 1).
+        colors: Optional[list[tuple[float]]]
+            A list of RGB values, specifying colors for each group.
+        legends: list[str]
+            Legends for each group.
+
+        """
+        if ax is None:
+            _, ax = plt.subplots(figsize=(6, 4))
+        num_groups = len(accs)
+        bar_width /= num_groups
+        CORRUPTIONS = {
+            'low': ['snow', 'frost', 'fog', 'brightness', 'contrast'],
+            'medium': ['motion_blur', 'zoom_blur', 'defocus_blur', 'glass_blur', 'elastic_transform', 'jpeg_compression', 'pixelate'],
+            'high': ['gaussian_noise', 'shot_noise', 'impulse_noise'],
+            'clean': ['clean'],
+        }
+        xticks = np.arange(4)
+        if colors is None:
+            colors = [matplotlib.cm.get_cmap('tab10')(x)[:3] for x in np.linspace(0, 1, num_groups)]
+        if legends is None:
+            legends = [f'Group {i}' for i in range(num_groups)]
+        bars = []
+        for i in range(num_groups):
+            color = colors[i]
+            acc_mean, acc_sem = np.zeros(4), np.zeros(4)
+            for j, category in enumerate(['low', 'medium', 'high', 'clean']):
+                _accs = []
+                for c in CORRUPTIONS[category]:
+                    if isinstance(accs[i][c], float):
+                        _accs += [accs[i][c]]
+                    else:
+                        _accs += list(accs[i][c])
+                acc_mean[j] = np.mean(_accs)*100.
+                acc_sem[j] = np.std(_accs)*100./len(CORRUPTIONS[category])**0.5
+            h = ax.bar(
+                xticks+(i-0.5*(num_groups-1))*bar_width,
+                acc_mean, width=bar_width, yerr=acc_sem, zorder=2, facecolor=color,
+            )
+            h.errorbar.get_children()[0].set_edgecolor(np.array(color)*0.8+0.2)
+            bars.append(h)
+        ax.legend(bars, legends, fontsize=12)
+        ylim = max(max(accs[i].values()) for i in range(num_groups))*100
+        ylim = -(-ylim//10)*10
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(['Low', 'Medium', 'High', 'Clean'])
+        ax.set_ylabel('Accuracy (%)')
+        ax.set_ylim([0, ylim])
         ax.grid(axis='y')
 
 
