d762524b90464bdbdf5abfbf6366ba04a923b7a6
Zhe Li <zhel@bcm.edu>
2022-12-20 13:28:18 -0600

    Bump version to 0.6.1

--- roarena/__init__.py
+++ roarena/__init__.py
@@ -1,5 +1,3 @@
-__version__ = '0.6.0'
-
 DEVICE = 'cuda'
 BATCH_SIZE = 100
 NUM_WORKERS = 4
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from roarena import __version__
 from setuptools import setup, find_packages
+
+with open('roarena/VERSION.txt', 'r') as f:
+    VERSION = f.readline().split('"')[1]
 
 setup(
     name="roarena",
-    version=__version__,
+    version=VERSION,
     author='Zhe Li',
-    python_requires='>=3',
+    python_requires='>=3.9',
     packages=find_packages(),
-    install_requires=['torch', 'jarvis', 'eagerpy', 'numba', 'foolbox'],
+    install_requires=['torch', 'jarvis>=0.5', 'eagerpy', 'numba', 'foolbox'],
 )
